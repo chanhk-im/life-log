932c9e0478d8151e7aab663ba75d2e0bbc1eb9db
chanhk-im <cfasd1875@gmail.com>
2024-11-12 11:48:22 +0000

    docker temp add

--- src/main/java/com/chanhk/lifelog/LifelogApplication.java
+++ src/main/java/com/chanhk/lifelog/LifelogApplication.java
@@ -2,12 +2,26 @@ package com.chanhk.lifelog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class LifelogApplication {
-
+@RestController
+public class LifelogApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(LifelogApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LifelogApplication.class);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
